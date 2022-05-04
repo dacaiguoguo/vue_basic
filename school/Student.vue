e54c9954d8c599cf1c3884d6beb5badf7dac91bf
sunyanguo <sunyanguo@lvmama.com>
2022-05-04 15:11:15 +0800

    单组件写法

---
+++ school/Student.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="demo">
+    <h3>{{ name }}</h3>
+    <h3>{{ age }}</h3>
+    <button @click="showName">点我提示学生名称</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Student",
+  data() {
+    return {
+      name: "好学生",
+      age: 18
+    };
+  },
+  methods: {
+      showName() {
+          alert(this.name)
+      }
+  }
+};
+</script>
+
+<style scope>
+.demo {
+  background-color: gray;
+}
+</style>
