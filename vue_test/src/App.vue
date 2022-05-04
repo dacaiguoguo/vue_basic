62aef851eb3fe20d6fa7645759b4aa09edc50e1e
sunyanguo <sunyanguo@lvmama.com>
2022-05-04 20:15:41 +0800

    vue_test 用脚手架创建

---
+++ vue_test/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <img src="./assets/logo.png" alt="logo">
+  <SchoolCom></SchoolCom>
+  <StudentCom></StudentCom>
+</div>
+</template>
+
+<script>
+import SchoolCom from "./components/SchoolCom.vue";
+import StudentCom from "./components/StudentCom.vue";
+export default {
+  name: "App",
+  components: {
+    SchoolCom,
+    StudentCom,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue_test/src/components/SchoolCom.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="demo">
+    <h3>{{ name }}</h3>
+    <h3>{{ add }}</h3>
+    <button @click="showName">点我提示学习名称</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  // name: "School",
+  data() {
+    return {
+      name: "好学校",
+      add: "好地址",
+    };
+  },
+  methods: {
+      showName() {
+          alert(this.name)
+      }
+  }
+};
+</script>
+
+<style scope>
+.demo {
+  background-color: orange;
+}
+</style>
