a6e495daf06cf4d2a638026942142ec65fae2f30
sunyanguo <sunyanguo@lvmama.com>
2022-05-05 10:32:44 +0800

    准备todolist

--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -1,5 +1,8 @@
 <template>
 <div>
+  <HeaderCom></HeaderCom>
+  <ListCom></ListCom>
+  <FooterCom></FooterCom>
   <img src="./assets/logo.png" alt="logo">
   <SchoolCom></SchoolCom>
   <StudentCom name="张三" age="18"></StudentCom>
@@ -10,11 +13,17 @@
 <script>
 import SchoolCom from "./components/SchoolCom.vue";
 import StudentCom from "./components/StudentCom.vue";
+import HeaderCom from "./components/HeaderCom.vue";
+import ListCom from "./components/ListCom.vue";
+import FooterCom from "./components/FooterCom.vue";
 export default {
   name: "App",
   components: {
     SchoolCom,
     StudentCom,
+    FooterCom,
+    ListCom,
+    HeaderCom
   },
 };
 </script>
--- vue_test/src/components/ItemCom.vue
+++ vue_test/src/components/ItemCom.vue
@@ -1,31 +1,30 @@
 <template>
   <div class="demo">
     <h3>{{ name }}</h3>
-    <h3>{{ add }}</h3>
-    <button @click="showName">点我提示学习名称</button>
+    <h3>{{ age + 1 }}</h3>
+    <button @click="showName">点我提示学生名称</button>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: "SchoolCom",
+  name: "StudentCom",
   data() {
     return {
-      name: "好学校",
-      add: "好地址",
     };
   },
   methods: {
       showName() {
           alert(this.name)
       }
-  }
+  },
+  props:['name', 'age']
 };
 </script>
 
 <style scope>
 .demo {
-  background-color: orange;
+  background-color: gray;
 }
 </style>
