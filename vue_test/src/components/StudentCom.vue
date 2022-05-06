715ca2058f43fd0ef041cc7318d6bde5926a731b
sunyanguo <sunyanguo@lvmama.com>
2022-05-06 21:52:08 +0800

    ok

--- vue_test/src/components/StudentCom.vue
+++ vue_test/src/components/StudentCom.vue
@@ -12,12 +12,8 @@ export default {
   methods: {
     giveName() {
       this.$emit('atguigu', this.studentName)
-      // this.$emit('atguigu', this.studentName);
       console.log('giveName')
       // 注意单引号是不行的 天坑啊  不是坑 原来是拼写错误了 aiguigu 和 atguigu
-      // this.$emit('atguigu', this.studentName);
-      // this.$emit('atguigu', '礼拜');
-      // this.$emit("atguigu", this.studentName);
     },
   },
 };
