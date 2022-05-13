3fccc3289442f8674ec9e3461c64df9d44c593fb
sunyanguo <sunyanguo@lvmama.com>
2022-05-13 15:49:59 +0800

    没法实现自动打开chrome

--- vue_test/vue.config.js
+++ vue_test/vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer:{
+    // open: true,
+    // 没法实现自动打开chrome
+    // app: {
+    //   name: 'google-chrome',
+    // },
+  },
+  lintOnSave:false,
 })
