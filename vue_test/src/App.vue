a055b25f141d397474142ef5b5e2bf4f9088a825
sunyanguo <sunyanguo@lvmama.com>
2022-05-13 16:45:12 +0800

    Update App.vue

--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="row">
-      <div class="col-xs-2 col-xs-offset-8">
+      <div class="col-xs-offset-2 col-xs-8">
         <div class="page-header"><h2>Vue Router Demo</h2></div>
       </div>
     </div>
@@ -9,9 +9,11 @@
       <div class="col-xs-2 col-xs-offset-2">
         <div class="list-group">
           <router-link class="list-group-item" active-class="active" to="/about"
-            >About</router-link>
-          <router-link class="list-group-item"  active-class="active" to="/home"
-            >Home</router-link>
+            >About</router-link
+          >
+          <router-link class="list-group-item" active-class="active" to="/home"
+            >Home</router-link
+          >
         </div>
       </div>
       <div class="col-xs-6">
