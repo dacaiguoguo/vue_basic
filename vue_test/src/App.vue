e20ba7952668a97951fe505cecc52cbf0941af66
sunyanguo <sunyanguo@lvmama.com>
2022-05-13 16:40:23 +0800

    实现路由功能了

--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -8,9 +8,9 @@
     <div class="row">
       <div class="col-xs-2 col-xs-offset-2">
         <div class="list-group">
-          <router-link class="list-group-item" active-class="active" to=""
+          <router-link class="list-group-item" active-class="active" to="/about"
             >About</router-link>
-          <router-link class="list-group-item" to=""
+          <router-link class="list-group-item"  active-class="active" to="/home"
             >Home</router-link>
         </div>
       </div>
